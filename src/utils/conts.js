91db1672675801f919d195070aa4050f4da19e8d
truongNguyen <nguyenphutruong1997@gmail.com>
2023-03-05 15:17:30 +0700

    Change backend link

--- src/utils/conts.js
+++ src/utils/conts.js
@@ -8,7 +8,8 @@ export const defaultProfileImage =
   "https://firebasestorage.googleapis.com/v0/b/stayfy-d4fc1.appspot.com/o/misc%2Fplaceholder-profile-image.png?alt=media&token=d7ee83a6-7b08-49e1-9d75-14de009335c9";
 
 //Railway backend URL
-export const railwayBackendURL = "https://stayfy-backend.onrender.com/";
+export const railwayBackendURL =
+  "https://stayfy-backend-production.up.railway.app/";
 
 //Render backend URL
 export const renderBackendUrl = "https://stayfy-backend.onrender.com/";
